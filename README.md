64458cb3e49255e3215cba8d1ceebc88942a6bd2
Chris Johnson <cjohnson@worthit.nl>
2019-07-04 14:19:57 +0200

    Final Code Clean Up Heroes component will be removed from App Shell. Keeping component but making no references to it.
    Part 1 Completed README.MD Updated

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ My personal attempt at Tour of Heros for Vue.Js
 cd vue-tour-of-heros
 npm run dev
 
+
+# The Hero Editor (Develop is the merged from this branch)
+This part of Tour Of Heros can be found in:
+Branch Title : 01-The-Hero-Editor
+
+# Displaying a List
+This will start with the end results from Branch : 01-The-Hero-Editor
+Branch Title : 02-Displaying-A-List
+
--- vue-tour-of-heros/src/App.vue
+++ vue-tour-of-heros/src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
     <h1>{{ title }}</h1>
-    <heroes />
+
+    <hero />
+
    <!--  <router-view/> -->
   </div>
 </template>
 
 <script>
-import Heroes from './components/heroes'
+
+import Hero from './components/hero'
 
 export default {
   name: 'App' ,
@@ -17,7 +20,7 @@ export default {
     }
   },
   components: {
-    Heroes
+    Hero
   }
 }
 </script>
@@ -31,4 +34,78 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+/* Global Styles */
+h1 {
+  color: #369;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 250%;
+}
+h2, h3 {
+  color: #444;
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: lighter;
+}
+body {
+  margin: 2em;
+}
+body, input[text], button {
+  color: #333;
+  font-family: Cambria, Georgia;
+}
+a {
+  cursor: pointer;
+  cursor: hand;
+}
+button {
+  font-family: Arial;
+  background-color: #eee;
+  border: none;
+  padding: 5px 10px;
+  border-radius: 4px;
+  cursor: pointer;
+  cursor: hand;
+}
+button:hover {
+  background-color: #cfd8dc;
+}
+button:disabled {
+  background-color: #eee;
+  color: #aaa;
+  cursor: auto;
+}
+
+/* Navigation link styles */
+nav a {
+  padding: 5px 10px;
+  text-decoration: none;
+  margin-right: 10px;
+  margin-top: 10px;
+  display: inline-block;
+  background-color: #eee;
+  border-radius: 4px;
+}
+nav a:visited, a:link {
+  color: #607D8B;
+}
+nav a:hover {
+  color: #039be5;
+  background-color: #CFD8DC;
+}
+nav a.active {
+  color: #039be5;
+}
+
+/* everywhere else */
+* {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
+
 </style>
