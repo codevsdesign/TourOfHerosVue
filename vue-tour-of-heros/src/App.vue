4adf8d40a1e4acceaaeda1edfbad6819f21f4880
Chris Johnson <cjohnson@worthit.nl>
2019-07-09 16:30:02 +0200

    Remove Hero from Loading in the Main App Remove space in the style portion of the heroes.vue file.

--- vue-tour-of-heros/src/App.vue
+++ vue-tour-of-heros/src/App.vue
@@ -20,8 +20,7 @@ export default {
     }
   },
   components: {
-    Hero,
-    Heroes
+      Heroes
   }
 }
 </script>
@@ -31,7 +30,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
